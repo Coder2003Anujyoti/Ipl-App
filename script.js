62b8309ad68ee84066e8d6c5b7a74c676b615eb3
Anujyoti De <anujyotide@gmail.com>
2024-11-30 13:17:42 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,81 @@
+var team=[{image:"Csk.webp",color:"yellow"},{image:"Kkr.webp",color:"purple"},{image:"Mi.webp",color:"skyblue"},{image:"Dc.webp",color:"blue"},{image:"Rcb.webp",color:"red"},
+{image:"Rr.webp",color:"hotpink"},{image:"Srh.webp",color:"orange"},{image:"Pbks.webp",color:"darkred"},{image:"Lsg.webp",color:"white"},
+{image:"Gt.webp",color:"silver"}];
+let player=[{image:"Gaikwad.webp",role:"Batsman",team:"Csk"},{image:"Conway.webp",role:"Batsman",team:"Csk"},{image:"Rizvi.webp",role:"Batsman",team:"Csk"},{image:"Dhoni.webp",role:"Batsman",team:"Csk"},{image:"Jadeja.webp",role:"All Rounder",team:"Csk"},{image:"Santner.webp",role:"All Rounder",team:"Csk"},{image:"Moeen.webp",role:"All Rounder",team:"Csk"},{image:"Dube.webp",role:"All Rounder",team:"Csk"},{image:"Rachin.webp",role:"All Rounder",team:"Csk"},{image:"Shardul.webp",role:"All Rounder",team:"Csk"},{image:"D.Mitchell.webp",role:"All Rounder",team:"Csk"},{image:"Deshpande.webp",role:"Bowler",team:"Csk"},{image:"D.Chahar.webp",role:"Bowler",team:"Csk"},{image:"Theekshana.webp",role:"Bowler",team:"Csk"},{image:"Pathirana.webp",role:"Bowler",team:"Csk"},{image:"S.Iyer.webp",role:"Batsman",team:"Kkr"},{image:"Rinku.webp",role:"Batsman",team:"Kkr"},{image:"Gurbaz.webp",role:"Batsman",team:"Kkr"},{image:"N.Rana.webp",role:"Batsman",team:"Kkr"},{image:"Salt.webp",role:"Batsman",team:"Kkr"},
+{image:"M.Pandey.webp"
+  ,role:"Batsman",team:"Kkr"},{image:"Angkrish.webp",role:"Batsman",team:"Kkr"},{image:"Ramandeep.webp",role:"All Rounder",team:"Kkr"},{image:"Russell.webp",role:"All Rounder",team:"Kkr"},{image:"V.Iyer.webp",role:"All Rounder",team:"Kkr"},{image:"Suyash.webp",role:"Bowler",team:"Kkr"},{image:"Starc.webp",role:"Bowler",team:"Kkr"},{image:"Narine.webp",role:"Bowler",team:"Kkr"},{image:"V.Chakravarthy.webp",role:"Bowler",team:"Kkr"},{image:"Harshit.webp",role:"Bowler",team:"Kkr"},{image:"Rohit.webp",role:"Batsman",team:"Mi"},{image:"Brevis.webp",role:"Batsman",team:"Mi"},{image:"Suryakumar.webp",role:"Batsman",team:"Mi"},{image:"I.Kishan.webp",role:"Batsman",team:"Mi"},{image:"Tilak.webp",role:"Batsman",team:"Mi"},{image:"T.David.webp",role:"Batsman",team:"Mi"},{image:"Hardik.webp",role:"All Rounder",team:"Mi"},{image:"Wadhera.webp",role:"All Rounder",team:"Mi"},{image:"Romario.webp",role:"All Rounder",team:"Mi"},{image:"Nabi.webp",role:"All Rounder",team:"Mi"},{image:"Arjun.webp",role:"All Rounder",team:"Mi"},{image:"Coetzee.webp",role:"Bowler",team:"Mi"},{image:"Bumrah.webp",role:"Bowler",team:"Mi"},{image:"Gopal.webp",role:"Bowler",team:"Mi"},{image:"Madhwal.webp",role:"Bowler",team:"Mi"},{image:"Pant.webp",role:"Batsman",team:"Dc"},{image:"Warner.webp",role:"Batsman",team:"Dc"},{image:"Stubbs.webp",role:"Batsman",team:"Dc"},{image:"Prithvi.webp",role:"Batsman",team:"Dc"},{image:"J.Freaser.webp",role:"Batsman",team:"Dc"},{image:"A.Porel.webp",role:"Batsman",team:"Dc"},{image:"Lalit.webp",role:"All Rounder",team:"Dc"},{image:"M.Marsh.webp",role:"All Rounder",team:"Dc"},{image:"Axar.webp",role:"All Rounder",team:"Dc"},{image:"Ishant.webp",role:"Bowler",team:"Dc"},{image:"Ostwal.webp",role:"Bowler",team:"Dc"},{image:"Nortje.webp",role:"Bowler",team:"Dc"},{image:"Mukesh.webp",role:"Bowler",team:"Dc"},{image:"Khaleel.webp",role:"Bowler",team:"Dc"},{image:"Kuldeep.webp",role:"Bowler",team:"Dc"},{image:"Kohli.webp",role:"Batsman",team:"Rcb"},{image:"Duplesis.webp",role:"Batsman",team:"Rcb"},{image:"Patidar.webp",role:"Batsman",team:"Rcb"},{image:"A.Rawat.webp",role:"Batsman",team:"Rcb"},{image:"DK.webp",role:"Batsman",team:"Rcb"},{image:"Jacks.webp",role:"Batsman",team:"Rcb"},{image:"Maxwell.webp",role:"All Rounder",team:"Rcb"},{image:"Lomror.webp",role:"All Rounder",team:"Rcb"},{image:"Green.webp",role:"All Rounder",team:"Rcb"},{image:"A.Deep.webp",role:"Bowler",team:"Rcb"},{image:"A.Joseph.webp",role:"Bowler",team:"Rcb"},{image:"Siraj.webp",role:"Bowler",team:"Rcb"},{image:"Ferguson.webp",role:"Bowler",team:"Rcb"},{image:"T.Curran.webp",role:"Bowler",team:"Rcb"},{image:"Y.Dayal.webp",role:"Bowler",team:"Rcb"},{image:"Markram.webp",role:"Batsman",team:"Srh"},{image:"Klaasen.webp",role:"Batsman",team:"Srh"},{image:"Head.webp",role:"Batsman",team:"Srh"},{image:"Samad.webp",role:"Batsman",team:"Srh"},{image:"Phillips.webp",role:"Batsman",team:"Srh"},{image:"Abhishek.webp",role:"All Rounder",team:"Srh"},{image:"Jansen.webp",role:"All Rounder",team:"Srh"},{image:"Sundar.webp",role:"All Rounder",team:"Srh"},{image:"N.K.Reddy.webp",role:"All Rounder",team:"Srh"},{image:"Cummins.webp",role:"Bowler",team:"Srh"},{image:"Bhuvneshwar.webp",role:"Bowler",team:"Srh"},{image:"Shahbaz.webp",role:"Bowler",team:"Srh"},{image:"Natarajan.webp",role:"Bowler",team:"Srh"},{image:"Umran.webp",role:"Bowler",team:"Srh"},{image:"Markande.webp",role:"Bowler",team:"Srh"},{image:"Samson.webp",role:"Batsman",team:"Rr"},{image:"Buttler.webp",role:"Batsman",team:"Rr"},{image:"Hetmyer.webp",role:"Batsman",team:"Rr"},{image:"Yashasvi.webp",role:"Batsman",team:"Rr"},{image:"Jurel.webp",role:"Batsman",team:"Rr"},{image:"Powell.webp",role:"Batsman",team:"Rr"},{image:"Parag.webp",role:"Batsman",team:"Rr"},{image:"Donovan.webp",role:"Batsman",team:"Rr"},{image:"Cadmore.webp",role:"Batsman",team:"Rr"},{image:"Ashwin.webp",role:"All Rounder",team:"Rr"},{image:"K.Sen.webp",role:"Bowler",team:"Rr"},{image:"Maharaj.webp",role:"Bowler",team:"Rr"},{image:"Chahal.webp",role:"Bowler",team:"Rr"},{image:"Sandeep.webp",role:"Bowler",team:"Rr"},{image:"Boult.webp",role:"Bowler",team:"Rr"},{image:"Dhawan.webp",role:"Batsman",team:"Pbks"},{image:"Jitesh.webp",role:"Batsman",team:"Pbks"},{image:"Bairstow.webp",role:"Batsman",team:"Pbks"},{image:"Prabhsimran.webp",role:"Batsman",team:"Pbks"},{image:"Livingstone.webp",role:"Batsman",team:"Pbks"},{image:"Shashank.webp",role:"Batsman",team:"Pbks"},{image:"Ashutosh.webp",role:"All Rounder",team:"Pbks"},{image:"S.Curran.webp",role:"All Rounder",team:"Pbks"},{image:"S.Raza.webp",role:"All Rounder",team:"Pbks"},{image:"Harshal.webp",role:"Bowler",team:"Pbks"},{image:"N.Ellis.webp",role:"Bowler",team:"Pbks"},{image:"R.Chahar.webp",role:"Bowler",team:"Pbks"},{image:"Arshdeep.webp",role:"Bowler",team:"Pbks"},{image:"Brar.webp",role:"Bowler",team:"Pbks"},{image:"Rabada.webp",role:"Bowler",team:"Pbks"},{image:"K.L.Rahul.webp",role:"Batsman",team:"Lsg"},{image:"DeKock.webp",role:"Batsman",team:"Lsg"},{image:"Pooran.webp",role:"Batsman",team:"Lsg"},{image:"Turner.webp",role:"Batsman",team:"Lsg"},{image:"Badoni.webp",role:"All Rounder",team:"Lsg"},{image:"Krunal.webp",role:"All Rounder",team:"Lsg"},{image:"Stoinis.webp",role:"All Rounder",team:"Lsg"},{image:"Hooda.webp",role:"All Rounder",team:"Lsg"},{image:"Shamar.webp",role:"Bowler",team:"Lsg"},{image:"Mayank.webp",role:"Bowler",team:"Lsg"},{image:"Bishnoi.webp",role:"Bowler",team:"Lsg"},{image:"Mohsin.webp",role:"Bowler",team:"Lsg"},{image:"Yash.webp",role:"Bowler",team:"Lsg"},{image:"Naveen.webp",role:"Bowler",team:"Lsg"},{image:"Henry.webp",role:"Bowler",team:"Lsg"},{image:"Gill.webp",role:"Batsman",team:"Gt"},{image:"Miller.webp",role:"Batsman",team:"Gt"},{image:"Wade.webp",role:"Batsman",team:"Gt"},{image:"W.Saha.webp",role:"Batsman",team:"Gt"},{image:"Williamson.webp",role:"Batsman",team:"Gt"},{image:"A.Manohar.webp",role:"All Rounder",team:"Gt"},{image:"Sudarshan.webp",role:"All Rounder",team:"Gt"},{image:"V.Shankar.webp",role:"All Rounder",team:"Gt"},{image:"Omarzai.webp",role:"All Rounder",team:"Gt"},{image:"Mohit.webp",role:"Bowler",team:"Gt"},{image:"N.Ahmad.webp",role:"Bowler",team:"Gt"},{image:"S.Kishore.webp",role:"Bowler",team:"Gt"},{image:"Rashid.webp",role:"Bowler",team:"Gt"},{image:"Shami.webp",role:"Bowler",team:"Gt"},{image:"Tewatia.webp",role:"Bowler",team:"Gt"}
+
+
+
+
+
+
+
+];
+var z=1;
+function roc1()
+{ let a=[1,2,3,4,5];
+  var k= document.getElementById("im"+z);
+  k.style.display="block";
+  a.splice(z-1,1);
+  for(let i=0;i<a.length;i++)
+    {  
+   document.getElementById("im"+a[i]).style.display="none";
+    }
+  if(z==5)
+  { z=1;
+  }
+  else
+  { z++;
+  }
+}
+setInterval(roc1,500);
+roc1();
+var j=1;
+function roc()
+{ let a=[1,2,3,4,5];
+  var k= document.getElementById("i"+j);
+  k.style.display="block";
+  a.splice(j-1,1);
+  for(let i=0;i<a.length;i++)
+    {  
+   document.getElementById("i"+a[i]).style.display="none";
+    }
+  if(j==5)
+  { j=1;
+  }
+  else
+  { j++;
+  }
+}
+setInterval(roc,500);
+roc();
+function back(){
+  window.location.reload();
+}
+function display(name){
+ var homepage=document.getElementById('homepage');
+  var playersections=document.getElementById('player-sections');
+  var profilepage=document.getElementById('profilepage');
+  var container=document.getElementById('team-container');
+  homepage.style.display="none";
+  profilepage.style.display="block";
+  team.forEach(icon);
+  function icon(item){
+ if(item.image===(name+".webp"))
+    { container.innerHTML=`<img src=${item.image}>`;
+      container.style.backgroundColor=`${item.color}`;
+    }
+  }
+ player.forEach(select);
+  function select(item){
+    if(item.team==name){
+      
+   playersections.innerHTML+=` <div id="player-profile">
+        <img src=${item.image}>
+        <div id="name"><h4>${item.image.slice(0,-5)}</h4></div>
+        <div id="role"><h4>${item.role}</h4></div>
+      </div>`;
+    }
+  }
+}
